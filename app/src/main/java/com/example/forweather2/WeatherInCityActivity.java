cd060f420b765a72a36b6868e091553beff81cba
i <rp893@yandex.ru>
2021-05-17 21:28:58 +0300

    7 comm

--- app/src/main/java/com/example/forweather2/WeatherInCityActivity.java
+++ app/src/main/java/com/example/forweather2/WeatherInCityActivity.java
@@ -10,5 +10,6 @@ public class WeatherInCityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_in_city);
+
     }
 }
